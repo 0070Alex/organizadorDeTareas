e2699978f340dc3ba6351b3ec28137bf4fea8600
alex <78516248+0070Alex@users.noreply.github.com>
2022-10-22 19:56:25 -0600

    se versiona front end

---
+++ Front-End/organizador-tareas/src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  BASE_API: "https://app-dase-gestor-actividades.herokuapp.com",
+  BASE_API_LOCAL: "http://localhost:8091",
+  BASE_API_TABLEROS_TAREAS: "https://service-tablero.herokuapp.com",
+  BASE_API_ACTIVIDADES: "https://app-dase-gestor-actividades.herokuapp.com",
+  BASE_API_COMENTARIOS: "https://app-dase-gestor-comentarios.herokuapp.com",
+  BASE_API_USUARIOS: "https://app-dase-gestor-usuarios.herokuapp.com",
+  BASE_API_CORREOS: "https://app-gestor-correos.herokuapp.com"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
